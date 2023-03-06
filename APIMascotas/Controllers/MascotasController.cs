a29839590705baea38e38a207ad978631da2788e
ana cossio <anacossio7@gmail.com>
2023-03-06 11:16:12 -0300

    project structure

---
+++ APIMascotas/Controllers/MascotasController.cs
@@ -0,0 +1,33 @@
+﻿using APIMascotas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace APIMascotas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotasController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+
+        public MascotasController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listMascotas = await _context.Mascotas.ToListAsync();
+                return Ok(listMascotas);
+            }
+            catch (Exception ex) { 
+                return BadRequest(ex.Message);
+            }
+            
+
+            
+        }
+    }
+}
